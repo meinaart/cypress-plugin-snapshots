12052f5dbb306c7ccd1fb2f33bdb6f4a920091a9
Debabrata Das <Debabrata.Das@group1001.com>
2018-11-17 07:00:05 -0500

    fix path typo

--- tasks/paths.js
+++ tasks/paths.js
@@ -4,5 +4,5 @@ module.exports = {
   PATH_DIFF_JS: 'diff2html/dist/diff2html.js',
   PATH_JS: './assets/script.js',
   PATH_SOCKET_JS: 'socket.io-client/dist/socket.io.js',
-  PATH_BASE64_JS: 'base64/base64.min.js'
+  PATH_BASE64_JS: 'js-base64/base64.min.js'
 };
