67056155157cec4d4bea0eb3de7330fd529ffa5b
kylie5505@gmail.com <kylie5505@gmail.com>
2019-10-11 22:59:56 -0400

    Cleaned up File.js

--- src/utils/File.js
+++ src/utils/File.js
@@ -6,15 +6,15 @@ const path = require('path');
 
 /**
  * Checks if the path exists.
- * @param {string} path - File/directory path to check.
+ * @param {string} pathToCheck - File/directory path to check.
  * @return {boolean} Whether or not the path exists.
 */
-function pathExists(path) {
-	try {
-		return fs.existsSync(path);
-	} catch (err) {
-		throw new Error('Unable to determine if file exists: ' + err);
-	}
+function pathExists(pathToCheck) {
+  try {
+    return fs.existsSync(pathToCheck);
+  } catch (err) {
+    throw new Error(`Unable to determine if file exists: ${err}`);
+  }
 }
 
 /**
@@ -22,59 +22,52 @@ function pathExists(path) {
  * @param {string} dirPath - Path to check.
 */
 function isDir(dirPath) {
-	try {
-		if (pathExists(dirPath)) {
-			try {
-				var stat = fs.lstatSync(dirPath);
-				return stat.isDirectory();
-			} catch (e) {
-				return false;
-			}
-		}
-		return !path.extname(dirPath);
-	} catch (err) {
-		throw new Error('Unable to determine if directory: ' + err);
-	}
+  try {
+    if (pathExists(dirPath)) {
+      try {
+        const stat = fs.lstatSync(dirPath);
+        return stat.isDirectory();
+      } catch (e) {
+        return false;
+      }
+    }
+    return !path.extname(dirPath);
+  } catch (err) {
+    throw new Error(`Unable to determine if directory: ${err}`);
+  }
 }
 
 /**
  * Recursively empties directory.
  * @param {string} folder - Path of folder to empty.
 */
-function removeEmptyFoldersRecursively(folder) {
-	function cleanFolderRecursively(folder) {
-		if (isDir(folder)) {
+function removeEmptyFoldersRecursively(folderToClean) {
+  function cleanFolderRecursively(folder) {
+    if (isDir(folder)) {
 
-			var files = fs.readdirSync(folder);
+      let files = fs.readdirSync(folder);
 
-			if (files.length > 0) {
-				files.forEach(function (file) {
-					cleanFolderRecursively(path.join(folder, file));
-				});
+      if (files.length > 0) {
+        files.forEach(function (file) {
+          cleanFolderRecursively(path.join(folder, file));
+        });
 
-				// re-evaluate files; after deleting subfolder parent folder might be empty
-				files = fs.readdirSync(folder);
-			}
+        // re-evaluate files; after deleting subfolder parent folder might be empty
+        files = fs.readdirSync(folder);
+      }
 
-			if (files.length == 0) {
-				folders.push(folder);
-				fs.rmdirSync(folder);
-				return;
-			}
-		}
-	}
+      if (files.length === 0) {
+        fs.rmdirSync(folder);
+      }
+    }
+  }
 
-	try {
-		var folders = []; // Folders that were removed
-		if (pathExists(folder)) {
-			cleanFolderRecursively(folder);
-		}
-		return folders;
-	} catch(err) {
-		console.err(err);
-	}	
+  if (pathExists(folderToClean)) {
+    cleanFolderRecursively(folderToClean);
+  }
+  return true;
 }
 
 module.exports = {
-	removeEmptyFoldersRecursively
-}
+  removeEmptyFoldersRecursively
+};
