5826b385bde90a3d0a43f50a0ba85fc0668cb4d5
Gonçalo Basto <1653045+gbasto@users.noreply.github.com>
2021-02-22 11:54:24 +0000

    Add retry-ability

--- src/commands/toMatchSnapshot.js
+++ src/commands/toMatchSnapshot.js
@@ -6,16 +6,28 @@ const logMessage = require('../utils/commands/logMessage');
 const { NO_LOG } = require('../constants');
 const { COMMAND_MATCH_SNAPSHOT: commandName } = require('./commandNames');
 
-function toMatchSnapshot(subject, options) {
+function toMatchSnapshot(subject, options, isRetry = false) {
   return getTaskData({
       commandName,
       options,
       subject,
+      isRetry,
     }).then(taskData => cy.task(
         MATCH_TEXT,
         taskData,
         NO_LOG
-      ).then(logMessage)
+      ).then((result) => {
+        if (!result.passed && options.retryCount > 0) {
+          return cy.wait(options.retryDelay).then(() => {
+            const newOptions = {
+              ...options,
+              retryCount: options.retryCount - 1,
+            };
+            toMatchSnapshot(subject, newOptions, true);
+          });
+        }
+        return logMessage(result);
+      })
     );
 }
 
