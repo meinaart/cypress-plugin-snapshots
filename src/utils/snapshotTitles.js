49ad47a3d586b26b53e900243525ccb436a84a7d
lina <linah.ahmed.92@gmail.com>
2022-07-11 23:05:58 +0200

    inclide attempt number in title canculation

--- src/utils/snapshotTitles.js
+++ src/utils/snapshotTitles.js
@@ -5,18 +5,28 @@ const SNAPSHOTS_IMAGE = {};
 
 const SNAPSHOT_TITLES_TEXT = [];
 const SNAPSHOT_TITLES_IMAGE = [];
+const SNAPSHOT_TEXT_ATTEMPTS = {};
+const SNAPSHOT_IMAGE_ATTEMPTS = {};
 
 function snapshotTitleIsUsed(snapshotTitle, isImage = false) {
   return (isImage ? SNAPSHOT_TITLES_IMAGE : SNAPSHOT_TITLES_TEXT).indexOf(snapshotTitle) !== -1;
 }
 
-function getSnapshotTitle(test, customName, customSeparator, isImage = false) {
+function getSnapshotTitle(test, customName, customSeparator, isImage = false,attemptNumber) {
   const name = customName || getTestTitle(test);
   const separator = customSeparator || ' #';
   const snapshots = isImage ? SNAPSHOTS_IMAGE : SNAPSHOTS_TEXT;
+  const snapshotsAttempts = isImage ? SNAPSHOT_IMAGE_ATTEMPTS : SNAPSHOT_TEXT_ATTEMPTS;
+  
+  const prevSnapshotTitle = `${name}${separator}${snapshots[name]||0}`;
+  if(!snapshotsAttempts[prevSnapshotTitle])snapshotsAttempts[prevSnapshotTitle]=0
 
   if (snapshots[name] !== undefined) {
-    snapshots[name] += 1;
+    if(snapshotsAttempts[prevSnapshotTitle] < attemptNumber) {
+      snapshotsAttempts[prevSnapshotTitle]=attemptNumber
+    }else{
+      snapshots[name] += 1;
+    }
   } else {
     snapshots[name] = 0;
   }
